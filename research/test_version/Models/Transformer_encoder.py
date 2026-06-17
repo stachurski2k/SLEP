f675f913ab676eeab376c29e93f863f43e80ac49
Mateusz Janeczek <m.janeczek777@gmail.com>
2026-06-17 22:13:53 +0200

    return to mean pooling on Transformer

--- research/test_version/Models/Transformer_encoder.py
+++ research/test_version/Models/Transformer_encoder.py
@@ -1,12 +1,7 @@
 import torch
 import torch.nn as nn
 
-
-import torch
-import torch.nn as nn
-
-
-class TransformerEncoder_model(nn.Module):
+class TransformerEncoder(nn.Module):
     def __init__(
         self,
         input_dim=144,
@@ -50,10 +45,8 @@ class TransformerEncoder_model(nn.Module):
         embedding_dim=64
 
         self.embedding_head = nn.Sequential(
-            nn.Linear(3*hidden_dim, 2*hidden_dim),
-            nn.GELU(),
-            nn.Linear(2*hidden_dim, hidden_dim),
-            nn.GELU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, embedding_dim),
         )
 
@@ -70,12 +63,8 @@ class TransformerEncoder_model(nn.Module):
 
         reconstructed = self.decoder(sequence) 
 
-        pooled_mean = sequence.mean(dim=1)  
-        pooled_min, _ = sequence.min(dim=1)  
-        pooled_max, _ = sequence.max(dim=1)  
-
-        embedding_combined = torch.cat((pooled_mean, pooled_min, pooled_max), dim=1)
-
-        embedding = self.embedding_head(embedding_combined)
+        pooled = sequence.mean(dim=1)  
+        embedding = self.embedding_head(pooled)  
 
         return reconstructed, embedding
+
