9391f2924eea4bd17c477084febf96fae8ec746d
efess3 <123400693+efess3@users.noreply.github.com>
2026-09-11 18:57:39 +0200

    feat(users/domain): add UserNotFoundError exception

--- src/language-learning-backend/modules/users/domain/exceptions.py
+++ src/language-learning-backend/modules/users/domain/exceptions.py
@@ -3,3 +3,9 @@ class InvalidEmailError(ValueError):
     Email exception when email is not correct
     """
     pass
+
+class UserNotFoundError(LookupError):
+    """
+    Error when user does not exist
+    """
+    pass
