5abe2ff30cd1c58430392fc5c0045ff5cfeee622
efess3 <123400693+efess3@users.noreply.github.com>
2026-09-11 14:09:45 +0200

    feat(users): add email value_object with validation

---
+++ src/language-learning-backend/modules/users/domain/exceptions.py
@@ -0,0 +1,5 @@
+class InvalidEmailError(ValueError):
+    """
+    Email exception when email is not correct
+    """
+    pass
--- src/language-learning-backend/modules/users/domain/value_objects.py
+++ src/language-learning-backend/modules/users/domain/value_objects.py
@@ -1,4 +1,8 @@
+import re
+
+from exceptions import InvalidEmailError
 from enum import Enum
+from dataclasses import dataclass
 
 class UserRole(str, Enum):
     """
@@ -10,3 +14,28 @@ class UserRole(str, Enum):
 class Theme(str, Enum):
     LIGHT = "light"
     DARK = "dark"
+
+
+@dataclass(frozen=True)
+class Email:
+    """
+    Value Object representing email address
+    Guarantees correctness and immutability.
+    """
+    value: str
+    
+    _EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+    
+    def __post_init__(self) -> None:
+        if not isinstance(self.value, str):
+            raise InvalidEmailError("Email must be a string.")
+        
+        normalized_value = self.value.strip().lower()
+        
+        if not self._EMAIL_REGEX.match(normalized_value):
+            raise InvalidEmailError(f"Invalid email address: '{self.value}'")
+       
+        object.__setattr__(self, "value", normalized_value)
+    
+    def __str__(self) -> str:
+        return self.value
